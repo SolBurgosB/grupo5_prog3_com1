fa731bb556cfaccda004b210f911c2eb9062ab63
SolBurgosB <sburgosbernasconi@udesa.edu.ar>
2025-09-15 19:07:09 -0300

    Creamos el botón de Favorito y de Sacar Favorito

--- src/components/TarjetaPelicula/TarjetaPelicula.js
+++ src/components/TarjetaPelicula/TarjetaPelicula.js
@@ -2,30 +2,63 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 class TarjetaPelicula extends Component {
-constructor(props){
-    super(props)
-    this.state={
-        dataPeliculas: props.peliculas,
-        verMas: false,
-        textoBoton: "Ver Más",
-    }
-}; 
-seleccionar(){
-    this.setState({
-        verMas: !this.state.verMas,
-        textoBoton: this.state.verMas ? "Ver más": "Ver menos"
-    })};
+    constructor(props) {
+        super(props)
+        this.state = {
+            dataPeliculas: props.peliculas,
+            verMas: false,
+            textoBoton: "Ver Más",
+        }
+    };
+    seleccionar() {
+        this.setState({
+            verMas: !this.state.verMas,
+            textoBoton: this.state.verMas ? "Ver más" : "Ver menos"
+        })
+    };
+    favorito(id) {
+        let peliculaTraida = localStorage.getItem("fav");
+        let favorito = JSON.parse(peliculaTraida);
 
-render() {
-return (
-    <div className=''>
-        <img src={`https://image.tmdb.org/t/p/w342${this.state.dataPeliculas.poster_path}`} alt={`Imagen de ${this.state.dataPeliculas.title}`} />
-        <h2> {this.state.dataPeliculas.title}</h2>
-        <button onClick={() => this.seleccionar()} className="more">{this.state.textoBoton}</button>
-        {this.state.verMas ? <p>{this.state.dataPeliculas.overview}</p>: <p></p>}
-        <Link to={`/peliculas/${this.state.dataPeliculas.id}`}><button>Ver detalle</button></Link>
-    </div>
-)}};
+        if (!favorito) {
+            favorito = [];
+        }
+
+        favorito.push(id);
+
+        let peliculasToString = JSON.stringify(favorito);
+        localStorage.setItem("fav", peliculasToString);
+
+        this.setState({ seleccionado: true });
+    }
+    sacarFavorito(id) {
+        let peliculaTraida = localStorage.getItem("fav")
+        let favorito = JSON.parse(peliculaTraida)
+        if (favorito) {
+            let nuevofavorito = favorito.filter((favId) => favId != id)
+            let peliculasToString = JSON.stringify(nuevofavorito)
+            localStorage.setItem("fav", peliculasToString)
+        }
+        this.setState({ seleccionado: false })
+    }
+    render() {
+        return (
+            <div className=''>
+                <img src={`https://image.tmdb.org/t/p/w342${this.state.dataPeliculas.poster_path}`} alt={`Imagen de ${this.state.dataPeliculas.title}`} />
+                <h2> {this.state.dataPeliculas.title}</h2>
+                <button onClick={() => this.seleccionar()} className="more">{this.state.textoBoton}</button>
+                {this.state.verMas ? <p>{this.state.dataPeliculas.overview}</p> : <p></p>}
+                <Link to={`/peliculas/${this.state.dataPeliculas.id}`}><button>Ver detalle</button></Link>
+                {
+                    this.state.seleccionado ?
+                        <button onClick={() => this.sacarFavorito(this.state.dataPeliculas.id)}>Sacar Favorito</button>
+                        :
+                        <button onClick={() => this.favorito(this.state.dataPeliculas.id)}>Favorito</button>
+                }
+            </div>
+        )
+    }
+};
 
 
 export default TarjetaPelicula;
