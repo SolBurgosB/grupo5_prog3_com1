964aea6ec597ce5792b57fd11c65f456d11d06b0
SolBurgosB <sburgosbernasconi@udesa.edu.ar>
2025-09-20 19:55:48 -0300

    Hicimos el HTML de SerieDetalle

--- src/components/SerieDetalle/SerieDetalle.js
+++ src/components/SerieDetalle/SerieDetalle.js
@@ -1,23 +1,28 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
-class SerieDetalle extends Component{
-  constructor(props){
+class SerieDetalle extends Component {
+  constructor(props) {
     super(props)
-    this.state={
+    this.state = {
       dataSeries: props.series,
     }
   }
-  render(){
-    return(
-              <div className="character-card">
-                  <img src={`https://image.tmdb.org/t/p/w342${this.state.dataSeries.poster_path}`} alt={`Imagen de ${this.state.dataSeries.name}`} />
-                  <h2> {this.state.dataSeries.name}</h2>
-                  <p>{this.state.dataSeries.popularity}</p>
-                  <p>{this.state.dataSeries.first_air_date}</p>
-                  <p>{this.state.dataSeries.overview}</p>
-                  
-              </div>
-          )
-    }}
+  render() {
+    return (
+      <div className="character-card">
+        <h2 className="alert alert-warning">{this.state.dataSeries.name}</h2>
+        <section class="row">
+          <img src={`https://image.tmdb.org/t/p/w342${this.state.dataSeries.poster_path}`} alt={`Imagen de ${this.state.dataSeries.name}`} />
+          <section class="col-md-6 info">
+            <h3>Descripción</h3>
+            <p className="description">{this.state.dataSeries.overview}</p>
+            <p className="mt-0 mb-0" id="release-date"><strong>Fecha de estreno:</strong>{this.state.dataSeries.first_air_date}</p>
+            <p>{this.state.dataSeries.popularity}</p>
+          </section>
+        </section>
+      </div>
+    )
+  }
+}
 export default SerieDetalle;
